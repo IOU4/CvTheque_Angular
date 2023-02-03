d2cf522bc5abe14b4de1ab650f09fb4f0e77dea7
Houssam-Eddine <94980688+HoussamEddineLamzoudi@users.noreply.github.com>
2023-02-03 12:39:58 +0100

    add a static cv-viewer

--- src/app/student/student-routing.module.ts
+++ src/app/student/student-routing.module.ts
@@ -1,3 +1,4 @@
+import { CvViewerComponent } from './cv-viewer/cv-viewer.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
@@ -7,7 +8,8 @@ const childRoutes: Routes = [
     path: 'student', children: [
       { path: 'dashboard', component: DashboardComponent }
     ]
-  }
+  },
+  { path: 'test', component: CvViewerComponent }
 ];
 
 @NgModule({
--- src/app/student/student.module.ts
+++ src/app/student/student.module.ts
@@ -4,18 +4,22 @@ import { StudenRoutingModule } from './student-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { MaterialExampleModule } from 'src/material.module';
 import { DocumentPreviewComponent } from './document-preview/document-preview.component';
+import { CvViewerComponent } from './cv-viewer/cv-viewer.component';
+import { NgxDocViewerModule } from 'ngx-doc-viewer';
 
 
 
 @NgModule({
   declarations: [
     DashboardComponent,
-    DocumentPreviewComponent
+    DocumentPreviewComponent,
+    CvViewerComponent
   ],
   imports: [
     CommonModule,
     StudenRoutingModule,
-    MaterialExampleModule
+    MaterialExampleModule,
+    NgxDocViewerModule
   ]
 })
 export class StudentModule { }
